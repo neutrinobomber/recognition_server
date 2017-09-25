7fdf632feb6955a93ddee756002844628864c094
nutrinobomber <neutrinobomber@users.noreply.github.com>
2017-09-25 22:26:49 +0300

    identity verification

--- start.py
+++ start.py
@@ -3,6 +3,24 @@ from io import BytesIO
 import face_recognition
 import cherrypy
 import base64
+import numpy
+
+
+def decode_image(encoded):
+    bits = base64.b64decode(encoded)
+    file = BytesIO(bits)
+    return file
+
+
+def encode_encoding(encoding):
+    encoded = base64.b64encode(encoding)
+    return encoded
+
+
+def decode_encoding(encoding):
+    decoded = base64.b64decode(encoding)
+    result = numpy.frombuffer(decoded)
+    return result
 
 
 def get_encodings(file):
@@ -10,6 +28,11 @@ def get_encodings(file):
     face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model='cnn')
     face_encodings = face_recognition.face_encodings(image, face_locations)
     return face_encodings
+
+
+def verify_identity(known_encoding, unknown_encoding):
+    results = face_recognition.compare_faces([known_encoding], unknown_encoding)
+    return results[0]
 
 
 def process_image(image):
@@ -33,19 +56,31 @@ class Server(object):
         if cherrypy.request.method == 'POST':
             data = cherrypy.request.json
             if data['image']:
-                bits = base64.b64decode(data['image'])
-                image = BytesIO(bits)
+                image = decode_image(data['image'])
                 processed = process_image(image)
                 encodings = get_encodings(processed)
 
                 if len(encodings) > 0:
-                    print(encodings[0])
-                    encoded = base64.b64encode(encodings[0])
-                    # decoded = base64.b64decode(encoded)
-                    # num = np.frombuffer(decoded)
-                    # print(num)
+                    encoded = encode_encoding(encodings[0])
                     return {'encoding': encoded}
-            return {'key': 'value'}
+        return {'message': 'error'}
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
+    def verify(self):
+        if cherrypy.request.method == 'POST':
+            data = cherrypy.request.json
+            if data['image'] is not None and data['encoding'] is not None:
+                image = decode_image(data['image'])
+                processed = process_image(image)
+                unknown_encodings = get_encodings(processed)
+                known_encoding = decode_encoding(data['encoding'])
+
+                if len(unknown_encodings) > 0:
+                    result = verify_identity(known_encoding, unknown_encodings[0])
+                    return {'same': str(result)}
+        return {'message': 'error'}
 
 
 if __name__ == '__main__':
